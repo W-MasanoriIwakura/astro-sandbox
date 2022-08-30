ace3e22dc81c5caca3679f8d7ceb73e85cee9e84
W-MasanoriIwakura <masanori.iwakura@want.jp>
2022-08-30 13:17:28 +0900

    Remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://W-MasanoriIwakura.github.io",
-  base: "/astro-sandbox",
   integrations: [mdx(), sitemap()],
 });
